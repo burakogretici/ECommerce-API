1747df198dac2688764806bd581c4261d57d9ed0
Burak Öğretici <burakogreticii@gmail.com>
2022-04-29 18:05:20 +0300

    get request added

--- Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.Net;
 using ECommerceAPI.Application.Repositories.Product;
+using ECommerceAPI.Application.RequestParameters;
 using ECommerceAPI.Application.ViewModels.Products;
 using ECommerceAPI.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -11,11 +12,14 @@ namespace ECommerceAPI.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductWriteRepository _context;
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
 
-        public ProductsController(IProductWriteRepository context)
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
         {
-            _context = context;
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+
         }
         [HttpPost]
         public async Task<IActionResult> Add(VM_Create_Product model)
@@ -27,9 +31,29 @@ namespace ECommerceAPI.API.Controllers
                 Price = model.Price
             };
 
-            await _context.InsertAsync(product);
-            await _context.SaveAsync();
+            await _productWriteRepository.InsertAsync(product);
+            await _productWriteRepository.SaveAsync();
             return StatusCode((int)HttpStatusCode.Created);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] Pagination pagination)
+        {
+            var totalCount = _productReadRepository.GetAll(false).Count();
+            var products = _productReadRepository.GetAll(false).Skip(pagination.Page * pagination.Size).Take(pagination.Size).Select(p => new
+            {
+                p.Id,
+                p.Name,
+                p.Stock,
+                p.Price,
+                p.CreatedDate,
+                p.UpdatedDate
+            }).ToList();
+
+            return Ok(new
+            {
+                totalCount,
+                products
+            });
         }
     }
 }
