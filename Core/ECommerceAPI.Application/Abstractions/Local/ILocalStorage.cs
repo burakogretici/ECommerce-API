f64e6043532bbacad8b0034408ef45dc4ce1aed9
burakogretici <burakogreticii@gmail.com>
2022-06-06 23:39:25 +0300

    added

---
+++ Core/ECommerceAPI.Application/Abstractions/Local/ILocalStorage.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceAPI.Application.Abstractions.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Abstractions.Local
+{
+    public interface ILocalStorage : IStorage
+    {
+    }
+}
---
+++ Core/ECommerceAPI.Application/Abstractions/Storage/Storage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Abstractions.Storage
+{
+    public interface Storage
+    {
+        public interface IStorageService : IStorage
+        {
+            public string StorageName { get; }
+        }
+    }
+}
